214a46c8ebe9102b046426e7dd817e944131b2f9
hesamjajo6 <hesamjafari06@gmail.com>
2026-08-06 16:57:05 -0700

    Add createdAt and updateAt to ConversationEntity

--- src/main/java/com/github/hesamjafari06/chat_server/entity/ConversationEntity.java
+++ src/main/java/com/github/hesamjafari06/chat_server/entity/ConversationEntity.java
@@ -1,8 +1,11 @@
 package com.github.hesamjafari06.chat_server.entity;
 
 import com.github.hesamjafari06.chat_server.enums.ConversationType;
+import com.github.hesamjafari06.chat_server.enums.UserStatus;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.Instant;
 
 @Entity
 @Table(name = "conversations")
@@ -18,7 +21,26 @@ public class ConversationEntity {
     private Long id;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false,updatable = false)
     private ConversationType type;
 
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(nullable = false)
+    private Instant updatedAt;
+
+    @PrePersist
+    public void onCreate(){
+        Instant now = Instant.now();
+        createdAt = now;
+        updatedAt = now;
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        Instant now = Instant.now();
+        updatedAt = now;
+    }
 
 }
