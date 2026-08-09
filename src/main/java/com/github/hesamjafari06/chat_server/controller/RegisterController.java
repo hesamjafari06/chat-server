5bb1023d0a318787754156e6ad3ee862f58f393b
hesamjajo6 <hesamjafari06@gmail.com>
2026-08-08 19:32:33 -0700

    Fixing the Auth bug

--- src/main/java/com/github/hesamjafari06/chat_server/controller/RegisterController.java
+++ src/main/java/com/github/hesamjafari06/chat_server/controller/RegisterController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RegisterController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @PostMapping
     public ResponseEntity<Void> register(@RequestBody CreateUserRequest request) {
