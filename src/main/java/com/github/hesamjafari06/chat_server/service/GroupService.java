dca524d54da5676c117b58d572b7347f007271f3
hesamjajo6 <hesamjafari06@gmail.com>
2026-08-12 12:39:27 -0700

    Add update group to interface

--- src/main/java/com/github/hesamjafari06/chat_server/service/GroupService.java
+++ src/main/java/com/github/hesamjafari06/chat_server/service/GroupService.java
@@ -1,6 +1,7 @@
 package com.github.hesamjafari06.chat_server.service;
 
 import com.github.hesamjafari06.chat_server.dto.request.CreateGroupRequest;
+import com.github.hesamjafari06.chat_server.dto.request.UpdateGroupRequest;
 import com.github.hesamjafari06.chat_server.dto.response.ApiResponse;
 import com.github.hesamjafari06.chat_server.dto.response.GroupResponse;
 import com.github.hesamjafari06.chat_server.entity.ConversationEntity;
@@ -18,4 +19,6 @@ public interface GroupService {
     GroupEntity getGroupByConversation(ConversationEntity conversation);
 
     void deleteGroup(GroupEntity group);
+
+    ApiResponse<GroupResponse> updateGroup(UpdateGroupRequest request);
 }
